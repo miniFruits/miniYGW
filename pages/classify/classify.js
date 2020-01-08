f4167fb406402714950061d2aaae2052d073b2ec
1991768641 <1991768641@qq.com>
2020-01-08 20:59:53 +0800

    2020-1-8-dingjian-晚间提交

--- pages/classify/classify.js
+++ pages/classify/classify.js
@@ -11,7 +11,9 @@ Component({
     num:1,
     indexflag:0,
     titlexindex:0,
-    contentlist:[]
+    contentlist:[],
+    tid:'0200000000',
+    allid:'0200000000'
   },
 
   lifetimes:{
@@ -20,6 +22,7 @@ Component({
       this.loadcontent()
     }
   },
+
 
   methods:{
     loadfirst(){  //加载首屏ID
@@ -35,10 +38,11 @@ Component({
         }
       })
     },
-    loadcontent(tid){  //加载具体内容
-      let newid = tid ||'0200000000';
+    loadcontent(){  //加载具体内容
+      let newid = this.data.tid;
+      let num=this.data.num;
       wx.request({
-        url: `http://api.egu365.com/goods/list?sorts=hits+asc&tid=${newid}&pageNo=${this.data.num}`,
+        url: `http://api.egu365.com/goods/list?sorts=hits+asc&tid=${newid}&pageNo=${num}`,
         success:(result)=>{
           this.setData({
             contentlist:result.data.list
@@ -65,7 +69,7 @@ Component({
       let list = this.data.biglist
       let title = list[this.data.indexflag].bcProductTypeEos
       let result = []
-      let obj = { tname: "全部", tid:"0200000000"}
+      let obj = { tname: "全部", tid:this.data.allid}
       result.push(obj)
       if (title) {
         for (var i = 0; i < title.length; i++) {
@@ -79,24 +83,53 @@ Component({
         })
       }
     },
+
     leftclick(e){   //左边列表单击事件
       this.setData({
         indexflag: e.currentTarget.dataset.indexs,
-        titlexindex: 0
+        tid: e.currentTarget.dataset.tid,
+        allid: e.currentTarget.dataset.tid,
+        titlexindex: 0,
+        num:1
       })
       this.titlefilter()
-      this.loadcontent(e.currentTarget.dataset.tid) //点击重新渲染内容
+      this.loadcontent() //点击重新渲染内容
     },
+
     titleclick(e){    //头部单击事件
       this.setData({
-        titlexindex:e.currentTarget.dataset.indexs
+        titlexindex:e.currentTarget.dataset.indexs,
+        tid: e.currentTarget.dataset.tid,
+        num:1
       })
-      this.loadcontent(e.currentTarget.dataset.tid) //点击重新渲染内容
+      this.loadcontent() //点击重新渲染内容
     },
     clicktodetails(e){    //点击跳转到详情页事件
       wx.navigateTo({
         url: '/pages/details/details?tid=' + e.currentTarget.dataset.tid
       })
+    },
+    lowdata(){      //加载更多数据
+      let newnum = this.data.num+1;
+      let newid = this.data.tid;
+      this.setData({
+        num: newnum
+      })
+      wx.request({
+        url: `http://api.egu365.com/goods/list?sorts=hits+asc&tid=${newid}&pageNo=${newnum}`,
+        success: (result) => {
+          console.log(result)
+          this.setData({
+            contentlist:[...this.data.contentlist,...result.data.list]
+          })
+        }
+      })
+    },
+    tosearch(){     //跳转到搜索页
+      wx.navigateTo({
+        url: '/component/Search/Search'
+      })
+
     }
   }
 })
