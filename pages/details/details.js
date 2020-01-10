7810611f8268dac088fefc79173ed010440a746f
仇公海 <1594611494@qq.com>
2020-01-10 22:12:20 +0800

    1-10-qgh

--- pages/details/details.js
+++ pages/details/details.js
@@ -1,41 +1,71 @@
-
 Component({
- 
   data: {
-    background:[],
+    background: [],
     indicatorDots: true,
     vertical: false,
     autoplay: false,
     interval: 2000,
     duration: 500,
-    youhui:'none'
+    youhui: "none",
+    info: {},
+    id: ""
   },
 
   lifetimes: {
-    attached:function(){
+    ready: function() {
+      this.setData({
+        id: this.options.tid
+      });
+      wx.setStorage({
+        key: "id",
+        data: this.options.tid
+      });
+
       // 轮播图
       wx.request({
-        url: 'http://api.egu365.com/goods/imgs?id=QV5gRRV4bxD80336ovH3',
-        success:(result)=>{
-          console.log(result.data.list)
+        url: "http://api.egu365.com/goods/imgs",
+        data: {
+          // id: this.data.id
+          id: "6157c98c7fd8445a9992"
+        },
+        success: result => {
           this.setData({
-            background:result.data.list
-          })
+            background: result.data.list
+          });
         }
-      })
+      });
+      // 商品信息
+      wx.request({
+        url: "http://api.egu365.com/goods/publish",
+        data: {
+          // id: this.data.id
+          id: "6157c98c7fd8445a9992"
+        },
+        success: result => {
+          this.setData({
+            info: {
+              goodsName: result.data.obj.goodsName,
+              param2: result.data.obj.bseGoodsEo.param2,
+              mallPrice: result.data.obj.mallPrice,
+              grossWeight: result.data.obj.bseGoodsEo.grossWeight,
+              goodsUnit: result.data.obj.bseGoodsEo.goodsUnit
+            }
+          });
+        }
+      });
     }
   },
 
   methods: {
-    youhuiShow: function (){
+    youhuiShow: function() {
       this.setData({
-        youhui:'block'
-      })
+        youhui: "block"
+      });
     },
-    youhuiHidden: function (){
+    youhuiHidden: function() {
       this.setData({
-        youhui: 'none'
-      })
+        youhui: "none"
+      });
     }
   }
-})
+});
