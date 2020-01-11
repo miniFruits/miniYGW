99768b8c1c482fdc34cb708af65e800ef1ffa8a2
ALLU <942108671@qq.com>
2020-01-11 22:17:37 +0800

    hlw-1-11

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,28 +5,28 @@ Component({
    * 页面的初始数据
    */
   data: {
-     classify:[],
-     swipper:[],
-     selected:'依谷热卖',
-     tid:'',
-     fruits:[],
-     bjImg:String,
-     imgSrcs: Array,
-     left:0,
-     suoyin:0
-     
+    classify: [],
+    swipper: [],
+    selected: '依谷热卖',
+    tid: '',
+    fruits: [],
+    bjImg: String,
+    imgSrcs: Array,
+    left: 0,
+    suoyin: 0
+
   },
-  lifetimes:{
+  lifetimes: {
     created() {
       wx.request({
-        url: 'http://api.egu365.com/goods/classify',
-        success: (result) => {
-          console.log(result.data.list)
-          this.setData({
-            classify: result.data.list
-          })
-        }
-      }),
+          url: 'http://api.egu365.com/goods/classify',
+          success: (result) => {
+            console.log(result.data.list)
+            this.setData({
+              classify: result.data.list
+            })
+          }
+        }),
         wx.request({
           url: 'http://api.egu365.com/news/adviseSeats?type=10',
           success: (result) => {
@@ -36,37 +36,42 @@ Component({
             })
           }
         })
-      
+
+    },
+    ready() {
+      wx.showTabBar({
+
+      })
     }
   },
-  methods:{
-    handleSelect:function(e){
+  methods: {
+    handleSelect: function(e) {
       console.log(e.currentTarget.dataset.i)
-     this.setData({
-       selected: e.currentTarget.dataset.type,
-       tid: e.currentTarget.dataset.tid,
-       bjImg: e.currentTarget.dataset.bgImg,
-       suoyin: e.currentTarget.dataset.index
-     })
-      if (e.currentTarget.dataset.i <= 5 && e.currentTarget.dataset.i >= 0){
-        console.log(e.currentTarget.dataset.i )
+      this.setData({
+        selected: e.currentTarget.dataset.type,
+        tid: e.currentTarget.dataset.tid,
+        bjImg: e.currentTarget.dataset.bgImg,
+        suoyin: e.currentTarget.dataset.index
+      })
+      if (e.currentTarget.dataset.i <= 5 && e.currentTarget.dataset.i >= 0) {
+        console.log(e.currentTarget.dataset.i)
         this.setData({
           left: -(18 + ~~e.currentTarget.dataset.i * 82)
         })
       }
-      if(e.currentTarget.dataset.i > 5 ){
+      if (e.currentTarget.dataset.i > 5) {
         this.setData({
-          left:-444
+          left: -444
         })
       }
-      if (e.currentTarget.dataset.i < 0){
+      if (e.currentTarget.dataset.i < 0) {
         this.setData({
           left: 0
         })
       }
-    
+
     }
-     
+
   }
   /**
    * 生命周期函数--监听页面加载
@@ -85,7 +90,7 @@ Component({
   /**
    * 生命周期函数--监听页面隐藏
    */
-  
+
   /**
    * 生命周期函数--监听页面卸载
 
