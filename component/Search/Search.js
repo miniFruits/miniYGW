5a6ef7168f27b6123de32968da5c3bad9b225118
1991768641 <1991768641@qq.com>
2020-01-12 21:14:07 +0800

    2020-1-12-dingjian-1

--- component/Search/Search.js
+++ component/Search/Search.js
@@ -11,13 +11,46 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    resultlist:'',
+    list:[]
+  },
+  
+  lifetimes: {
+    attached: function () {
+      wx.request({
+        url: 'http://api.egu365.com/goods/hotWords?seat=109',
+        success:(res)=> {
+          this.setData({
+            list:res.data.list
+          })
+        }
+      })
+    }
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    req(){    //请求数据
+      wx.navigateTo({
+        url: '/pages/searchresult/searchresult?key=' + this.data.resultlist
+      })
+    },
+    cancel(){   //取消搜索
+      wx.navigateBack({
+        delta: 1
+      })
+    },
+    feedin(e){    //监听输入
+      this.data.resultlist = e.detail.value;
+    },
+    submitbutton(e){    //完成提交
+      this.req()
+    },
+    hot(e){    //热门点击搜索
+      this.setData({
+        resultlist: e.currentTarget.dataset.valueitem 
+      })
+      this.req()
+    }
   }
 })
+ 
